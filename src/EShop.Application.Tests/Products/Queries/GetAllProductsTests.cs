2be2ea9b691441ce20ca4a2fa20d5584adf87252
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-07-10 23:25:51 +0300

    Добавить тестовый проект для Application

---
+++ src/EShop.Application.Tests/Products/Queries/GetAllProductsTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Threading;
+using EShop.Application.Products.Queries;
+using Xunit;
+
+namespace EShop.Application.Tests.Products.Queries
+{
+    public class GetAllProductsTests : TestBase
+    {
+        [Fact]
+        public async void GetAllProducts_Nothing_ProductsNotEmpty()
+        {
+            var cmd = new GetAllProductsQuery();
+
+            var handler = new GetAllProductsQueryHandler(GetDbContext());
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.Products.Any());
+            Assert.True(result.Products.Length == 2);
+        }
+    }
+}
