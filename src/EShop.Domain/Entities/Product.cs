624eb19ffbdfa6c61a8ed4339828059346bad0fd
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-07-10 22:31:21 +0300

    Добавить базову сущность товара

---
+++ src/EShop.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EShop.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
