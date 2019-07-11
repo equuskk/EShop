e6dd812ad5c382317422b4a1d87118bc2784b05f
Kirill Kuznetsov <kirill.kuzn98@gmail.com>
2019-07-11 22:39:17 +0300

    Исправить логику GetProductById

--- src/EShop.Application/Products/Queries/GetProductById/GetProductByIdHandler.cs
+++ src/EShop.Application/Products/Queries/GetProductById/GetProductByIdHandler.cs
@@ -1,12 +1,10 @@
-﻿using EShop.DataAccess;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EShop.DataAccess;
 using EShop.Domain.Entities;
+using EShop.Domain.Exceptions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace EShop.Application.Products.Queries.GetProductById
 {
@@ -21,8 +19,14 @@ namespace EShop.Application.Products.Queries.GetProductById
 
         public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
         {
-            var product = await _db.Products.FirstOrDefaultAsync(x => x.Id == request.Id);
-            return product == null? throw new ArgumentException(): product;
+            var product = await _db.Products.FirstOrDefaultAsync(x => x.Id == request.Id,
+                                                                 cancellationToken);
+            if(product is null)
+            {
+                throw new NotFoundException(nameof(Product), request.Id);
+            }
+
+            return product;
         }
     }
 }
