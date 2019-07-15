a59040d2e1d22c6358cfdd3460949db0b9a60010
equus <kirill.kuzn98@gmail.com>
2019-07-15 16:03:23 +0300

    Добавить новые поля юзеров в БД

--- src/EShop.DataAccess/Migrations/UsersDb/20190715130303_AddNewFields.Designer.cs
+++ src/EShop.DataAccess/Migrations/UsersDb/20190715130303_AddNewFields.Designer.cs
@@ -4,14 +4,16 @@ using EShop.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EShop.DataAccess.Migrations.UsersDb
 {
     [DbContext(typeof(UsersDbContext))]
-    partial class UsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190715130303_AddNewFields")]
+    partial class AddNewFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
