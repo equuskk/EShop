55869a6c9046d2e9486947bec95b05239ff9a0fb
equus <kirill.kuzn98@gmail.com>
2019-07-15 16:17:57 +0300

    Добавить JWT авторизацию

---
+++ src/EShop.Application/Users/Services/GenerateTokenService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using EShop.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EShop.Application.Users.Services
+{
+    public class GenerateTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public GenerateTokenService(IConfiguration configuration) //TODO: IOptions
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateJwtToken(ShopUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtAuth:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtAuth:ExpireDays"]));
+
+            var token = new JwtSecurityToken(
+                                             _configuration["JwtAuth:Issuer"],
+                                             _configuration["JwtAuth:Issuer"],
+                                             claims,
+                                             expires: expires,
+                                             signingCredentials: creds
+                                            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- src/EShop.WebApi/Startup.cs
+++ src/EShop.WebApi/Startup.cs
@@ -1,8 +1,13 @@
-﻿using System.Reflection;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection;
+using System.Text;
 using EShop.Application.Products.Queries.GetAllProducts;
+using EShop.Application.Users.Services;
 using EShop.DataAccess;
 using EShop.Domain.Entities;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -10,6 +15,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EShop.WebApi
 {
@@ -36,6 +43,30 @@ namespace EShop.WebApi
             services.AddMediatR(typeof(GetAllProductsQuery).GetTypeInfo().Assembly);
 
             services.AddSwaggerDocument();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtAuth:Issuer"],
+                        ValidAudience = Configuration["JwtAuth:Issuer"],
+                        IssuerSigningKey =
+                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtAuth:Key"])),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                });
+            services.AddScoped<GenerateTokenService>();
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
