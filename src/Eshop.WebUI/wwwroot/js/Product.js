ecb8d7951ca9244c0ca4392f3049b89c9e62689b
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-08-04 14:23:30 +0300

    Изменить JS функцию.

---
+++ src/Eshop.WebUI/wwwroot/js/Product.js
@@ -0,0 +1,14 @@
+﻿
+function AddProduct(id) {
+
+        $.ajax({
+            type: 'POST',
+            url: 'Products/AddProductInCart/' + id,
+            success: function(data) {
+                var value = $("#amount").text();
+                value = parseInt(value) + 1;
+                $('#amount').html(value);
+            }
+        });
+};
+
