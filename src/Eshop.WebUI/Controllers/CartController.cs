ba30e40e5a3a2729ea7bd24af5d1a6be81ce4239
Anton-Volchkov <anton.volchkov.2001@gmail.com>
2019-08-04 14:22:44 +0300

    Добавить контроллер корзины.

---
+++ src/Eshop.WebUI/Controllers/CartController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EShop.Application.Cart.Queries.GetUserCart;
+using EShop.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShop.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly Helpers.Helpers _helpers;
+        private readonly IMediator _mediator;
+        private readonly UserManager<ShopUser> _manager;
+
+        public CartController(IMediator mediator, Helpers.Helpers helpers, UserManager<ShopUser> manager)
+        {
+            _mediator = mediator;
+            _helpers = helpers;
+            _manager = manager;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _manager.GetUserAsync(User);
+            var product = await _mediator.Send(new GetUserCartQuery(user.Id));
+            return View(product);
+        }
+    }
+}
